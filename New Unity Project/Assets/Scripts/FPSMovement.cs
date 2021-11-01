bbc226fb518fe85acda24f9b603a7bc6a5c9e732
Buvude <deisenb1@student.fitchburgstate.edu>
2021-11-01 13:43:40 -0400

    Got VB working!

--- New Unity Project/Assets/Scripts/FPSMovement.cs
+++ New Unity Project/Assets/Scripts/FPSMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FPSMovement : MonoBehaviour
 {
     public float speed = 10;
-    private float horz, vert;
+    private float horz, vert,straf;
     public GameObject Player;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,8 @@ public class FPSMovement : MonoBehaviour
     {
         horz = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(0, 0, vert) * (speed * Time.deltaTime));
+        straf = Input.GetAxis("Strafe");
+        transform.Translate(new Vector3(straf, 0, vert) * (speed * Time.deltaTime));
         transform.Rotate(new Vector3(0f, horz, 0f)*(speed*10*Time.deltaTime));
     }
 }
